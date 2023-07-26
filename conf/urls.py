ae0145b31d19a3f2bd7d6d87ba07aa644e792a3e
dunicode <dunicode@MacBook-Pro-de-dunicode.local>
2023-07-25 23:41:30 -0400

    feat: register users

--- conf/urls.py
+++ conf/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.urls import path
+from .views import RegisterView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='auth_register'),
 ]
